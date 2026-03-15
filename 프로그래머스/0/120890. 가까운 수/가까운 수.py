e211906791e036b7ff01e145965dce3df8afe321
yunji cho <132728613+YUMZII@users.noreply.github.com>
2026-03-15 14:30:35 +0900

    [level 0] Title: 가까운 수, Time: 0.02 ms, Memory: 9.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.py
@@ -0,0 +1,9 @@
+def solution(array, n):
+    answer =array[0]
+    
+    for num in array:
+        if abs(num - n) < abs(answer - n):
+            answer = num
+        elif abs(num - n) == abs(answer - n) and num < answer:
+            answer = num
+    return answer
